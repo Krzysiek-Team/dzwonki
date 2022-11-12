7064d6558e8b2291c287a5693730c09dead905c2
XceptDev <jakowlukvito@gmail.com>
2022-11-12 11:06:54 +0100

    Create dzwonki.py

---
+++ dzwonki.py
@@ -0,0 +1,148 @@
+import time
+from datetime import datetime
+
+now = datetime.now()
+
+current_time = now.strftime("%H:%M")
+print("Current Time =", current_time)
+
+while True:
+    from tabulate import tabulate
+    now = datetime.now()
+
+    current_time = now.strftime("%H:%M")
+    print("Godzina =", current_time)
+
+    czas = current_time
+#1
+    dzwonek1 = "8:00"
+    dzwonek2 = "8:45"
+#2
+    dzwonek3 = "8:55"
+    dzwonek4 = "9:40"
+#3
+    dzwonek5 = "9:50"
+    dzwonek6 = "10:35"
+#4
+    dzwonek7 = "10:45"
+    dzwonek8 = "11:30"
+#5
+    dzwonek9 = "11:50"
+    dzwonek10 = "12:35"
+#6
+    dzwonek11 = "12:55"
+    dzwonek12 = "13:40"
+#7
+    dzwonek13 = "14:00"
+    dzwonek14 = "14:45"
+#8
+    dzwonek15 = "14:50"
+    dzwonek16 = "15:35"
+#9
+    dzwonek17 = "15:40"
+    dzwonek18 = "16:25"
+#10
+    dzwonek19 = "16:30"
+    dzwonek20 = "17:15"
+    if czas == dzwonek1: # or dzwonek2 or dzwonek3 or dzwonek4 or dzwonek5
+        from pydub import AudioSegment
+        from pydub.playback import play
+        song = AudioSegment.from_wav(r'C:\Users\krzys\OneDrive\Pulpit\dzwonek.wav')
+        
+        play(song)
+        time.sleep(40)
+        pass
+
+    if czas == dzwonek2: # or dzwonek2 or dzwonek3 or dzwonek4 or dzwonek5
+        from pydub import AudioSegment
+        from pydub.playback import play
+        song = AudioSegment.from_wav(r'C:\Users\krzys\OneDrive\Pulpit\dzwonek.wav')
+        play(song)
+        time.sleep(40)
+        pass
+
+    if czas == dzwonek3: # or dzwonek2 or dzwonek3 or dzwonek4 or dzwonek5
+        from pydub import AudioSegment
+        from pydub.playback import play
+        song = AudioSegment.from_wav(r'C:\Users\krzys\OneDrive\Pulpit\dzwonek.wav')
+        play(song)
+        time.sleep(40)
+        pass
+
+    if czas == dzwonek4: # or dzwonek2 or dzwonek3 or dzwonek4 or dzwonek5
+        from pydub import AudioSegment
+        from pydub.playback import play
+        song = AudioSegment.from_wav(r'C:\Users\krzys\OneDrive\Pulpit\dzwonek.wav')
+        play(song)
+        time.sleep(40)
+        pass
+
+
+    if czas == dzwonek5: # or dzwonek2 or dzwonek3 or dzwonek4 or dzwonek5
+        from pydub import AudioSegment
+        from pydub.playback import play
+        song = AudioSegment.from_wav(r'C:\Users\krzys\OneDrive\Pulpit\dzwonek.wav')
+        play(song)
+        time.sleep(40)
+        pass
+
+
+    if czas == dzwonek6: # or dzwonek2 or dzwonek3 or dzwonek4 or dzwonek5
+        from pydub import AudioSegment
+        from pydub.playback import play
+        song = AudioSegment.from_wav(r'C:\Users\krzys\OneDrive\Pulpit\dzwonek.wav')
+        play(song)
+        time.sleep(40)
+        pass
+
+
+    if czas == dzwonek7: # or dzwonek2 or dzwonek3 or dzwonek4 or dzwonek5
+        from pydub import AudioSegment
+        from pydub.playback import play
+        song = AudioSegment.from_wav(r'C:\Users\krzys\OneDrive\Pulpit\dzwonek.wav')
+        play(song)
+        time.sleep(40)
+        pass
+
+    if czas == dzwonek8: # or dzwonek2 or dzwonek3 or dzwonek4 or dzwonek5
+        from pydub import AudioSegment
+        from pydub.playback import play
+        song = AudioSegment.from_wav(r'C:\Users\krzys\OneDrive\Pulpit\dzwonek.wav')
+        play(song)
+        time.sleep(40)
+        pass
+
+    if czas == dzwonek9: # or dzwonek2 or dzwonek3 or dzwonek4 or dzwonek5
+        from pydub import AudioSegment
+        from pydub.playback import play
+        song = AudioSegment.from_wav(r'C:\Users\krzys\OneDrive\Pulpit\dzwonek.wav')
+        play(song)
+        time.sleep(40)
+        pass
+
+    if czas == dzwonek10: # or dzwonek2 or dzwonek3 or dzwonek4 or dzwonek5
+        from pydub import AudioSegment
+        from pydub.playback import play
+        song = AudioSegment.from_wav(r'C:\Users\krzys\OneDrive\Pulpit\dzwonek.wav')
+        play(song)
+        time.sleep(40)
+        pass
+
+    if czas == dzwonek11: # or dzwonek2 or dzwonek3 or dzwonek4 or dzwonek5
+        from pydub import AudioSegment
+        from pydub.playback import play
+        song = AudioSegment.from_wav(r'C:\Users\krzys\OneDrive\Pulpit\dzwonek.wav')
+        play(song)
+        time.sleep(40)
+        pass
+
+    if czas == dzwonek12: # or dzwonek2 or dzwonek3 or dzwonek4 or dzwonek5
+        from pydub import AudioSegment
+        from pydub.playback import play
+        song = AudioSegment.from_wav(r'C:\Users\krzys\OneDrive\Pulpit\dzwonek.wav')
+        play(song)
+        time.sleep(40)
+        pass
+
+   
+    pass        
